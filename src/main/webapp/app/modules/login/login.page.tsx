50d1ae81cfa9eb40cf4351ebd873b4fc4b7b6485
Mohamed Abdel Moneim <Eachawy@gmail.com>
2025-01-25 12:05:13 +0400

    install prime react

--- src/main/webapp/app/modules/login/login.page.tsx
+++ src/main/webapp/app/modules/login/login.page.tsx
@@ -1,15 +1,35 @@
 import React from "react";
 import { translate, Translate } from "react-jhipster";
+import { InputText } from "primereact/inputtext";
+import { Dropdown } from "primereact/dropdown";
 
 export const LoginPage = () => {
   {/* prettier-ignore */ }
   // const account = useAppSelector(state => state.authentication.account);
+  const [value, setValue] = React.useState("");
+  const [selectedCity, setSelectedCity] = React.useState({});
+  const cities = [
+    { name: "New York", code: "NY" },
+    { name: "Rome", code: "RM" },
+    { name: "London", code: "LDN" },
+    { name: "Istanbul", code: "IST" },
+    { name: "Paris", code: "PRS" },
+  ];
 
   return (
     <div>
       Login Page
       {translate("home.subtitle")}
       <Translate contentKey="home.subtitle">This is your homepage</Translate>
+      <InputText value={value} onChange={(e) => setValue(e.target.value)} />
+      <Dropdown
+        value={selectedCity}
+        onChange={(e: any) => setSelectedCity(e.value)}
+        options={cities}
+        optionLabel="name"
+        placeholder="Select a City"
+        className="w-full md:w-14rem"
+      />
     </div>
   );
 };
