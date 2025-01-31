25e07b7970bc310af03ae3238479527ba30d9111
Mohamed Abdel Moneim <Eachawy@gmail.com>
2025-01-31 17:17:26 +0400

    convert to use form components

--- src/main/webapp/app/modules/new-profile/create-new-profile/pages/create-new-file/createNewProfile.page.tsx
+++ src/main/webapp/app/modules/new-profile/create-new-profile/pages/create-new-file/createNewProfile.page.tsx
@@ -1,12 +1,12 @@
-/* eslint-disable prettier/prettier */
+/* eslint-disable */
+// prettier-ignore
 import BreadcrumbComponent from "app/shared/components/breadcrumb.component";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { translate } from "react-jhipster";
 import CreateNewProfileStepsComponent from "../../../Shared/createNewProfileSteps.component";
-import { RadioButton } from 'primereact/radiobutton';
-import { InputText } from "primereact/inputtext";
-import { Dropdown } from "primereact/dropdown";
 import { useNavigate } from "react-router";
+import { useForm } from "react-hook-form";
+import { ButtonComponent, DropDownComponent, InputComponent, RadioButtonComponent } from "@eachawy/frontend-library";
 
 interface Country {
     name: string;
@@ -14,30 +14,27 @@ interface Country {
 }
 
 const CreateNewProfilePage = () => {
-    const [fileType, setFileType] = useState('')
-
-    const [NameAr, setNameAr] = useState('');
-    const [NameEn, setNameEn] = useState('');
-    const [nationalNumber, setNationalNumber] = useState('');
-    const [address, setAddress] = useState('');
-    const [phoneNumber, setPhoneNumber] = useState('');
-    const [email, setEmail] = useState('');
-    const [selectedCountryCode, setSelectedCountryCode] = useState('')
-
 
     const navigate = useNavigate();
+    const { register, handleSubmit, formState: { errors }, watch, setValue, getValues } = useForm({ mode: 'onTouched', });
+    useEffect(() => {
+        setValue('companyType', 'corporateType');
+    }, []);
+
+
     const countryCode: Country[] = [
         { name: '+962', code: 'ORD' },
         { name: '+20', code: 'EGY' },
     ]
 
-    const saveAndCloseFn = () => {
+    const saveAndCloseFn = () => { };
 
-    }
-
-    const nextFn = () => {
+    const handleLogin = (data) => {
+        console.log(data);
         navigate("/select-file-type");
     };
+
+
 
     return (
         <div className="createNewProfilePage">
@@ -47,57 +44,132 @@ const CreateNewProfilePage = () => {
                 <label className="serialNo">{translate("createNewProfile.serialNumber")}<span>1256543</span></label>
 
                 <div className="radioButtonDiv">
-                    <div className="flex align-items-center">
-                        <RadioButton inputId="fileType2" name="corporate" value="corporate" onChange={(e) => setFileType(e.value)} checked={fileType === 'corporate'} />
-                        <label htmlFor="fileType2" className="ml-2">{translate("createNewProfile.companyTypeCorporate")}</label>
-                    </div>
-                    <div className="flex align-items-center">
-                        <RadioButton inputId="fileType1" name="personal" value="personal" onChange={(e) => setFileType(e.value)} checked={fileType === 'personal'} />
-                        <label htmlFor="fileType1" className="ml-2">{translate("createNewProfile.companyTypePersonal")}</label>
-                    </div>
+                    <RadioButtonComponent
+                        name="companyType"
+                        label={translate("createNewProfile.companyTypeCorporate")}
+                        register={register}
+                        errors={errors}
+                        value={'corporateType'}
+                        watch={watch}
+                        onChange={(e) => { setValue("companyType", e.value === 'corporateType' ? 'corporateType' : 'personalType'); }}
+                        checked={getValues().companyType === 'corporateType'}
+                    />
+                    <RadioButtonComponent
+                        name="companyType"
+                        label={translate("createNewProfile.companyTypePersonal")}
+                        register={register}
+                        errors={errors}
+                        value={'personalType'}
+                        watch={watch}
+                        onChange={(e) => { setValue("companyType", e.value === 'corporateType' ? 'corporateType' : 'personalType'); }}
+                        checked={getValues().companyType === 'personalType'}
+                    />
                 </div>
 
                 <div className="profileDataContainer">
-                    <div>
-                        <label>{translate("createNewProfile.companyNameAr", {
-                            name: fileType === "corporate" ? translate("createNewProfile.company") :
-                                translate("createNewProfile.person")
-                        })}<span>*</span></label>
-                        <InputText placeholder={translate("createNewProfile.companyNameAr", {
-                            name: fileType === "corporate" ? translate("createNewProfile.company") :
-                                translate("createNewProfile.person")
-                        })} value={NameAr} onChange={(e) => setNameAr(e.target.value)} />
-                    </div>
-                    <div>
-                        <label>{translate("createNewProfile.companyNameEn", {
-                            name: fileType === "corporate" ? translate("createNewProfile.company") :
-                                translate("createNewProfile.person")
-                        })}<span>*</span></label>
-                        <InputText placeholder={translate("createNewProfile.companyNameEn", {
-                            name: fileType === "corporate" ? translate("createNewProfile.company") :
-                                translate("createNewProfile.person")
-                        })} value={NameEn} onChange={(e) => setNameEn(e.target.value)} />
-                    </div>
-                    <div>
-                        <label>{translate("createNewProfile.nationalNumber")}<span>*</span></label>
-                        <InputText placeholder={translate("createNewProfile.exm") + "1234567"} className="nationalNoInput" value={nationalNumber} onChange={(e) => setNationalNumber(e.target.value)} />
-                    </div>
-                    <div>
-                        <label>{translate("createNewProfile.address")}<span>*</span></label>
-                        <InputText placeholder={translate("createNewProfile.enterTheAddress")} value={address} onChange={(e) => setAddress(e.target.value)} />
-                    </div>
+                    <InputComponent
+                        id="NameAr"
+                        type="NameAr"
+                        name="NameAr"
+                        label={
+                            (getValues().companyType === 'corporateType') ?
+                                translate("createNewProfile.companyNameAr") : translate("createNewProfile.personNameAr")}
+                        placeholder={
+                            (getValues().companyType === 'corporateType') ?
+                                translate("createNewProfile.companyNameAr") : translate("createNewProfile.personNameAr")}
+                        register={register}
+                        rules={{ required: 'You must enter your first name.' }}
+                        errors={errors}
+                        setValueMethod={setValue}
+                        watch={watch}
+                        onChange={(e) => setValue("NameAr", e.target.value)}
+                    />
+                    <InputComponent
+                        id="NameEn"
+                        type="NameEn"
+                        name="NameEn"
+                        label={
+                            (getValues().companyType === 'corporateType') ?
+                                translate("createNewProfile.companyNameEn") : translate("createNewProfile.personNameEn")}
+                        placeholder={
+                            (getValues().companyType === 'corporateType') ?
+                                translate("createNewProfile.companyNameEn") : translate("createNewProfile.personNameEn")}
+                        register={register}
+                        rules={{ required: 'You must enter your first name.' }}
+                        errors={errors}
+                        setValueMethod={setValue}
+                        watch={watch}
+                        onChange={(e) => setValue("NameEn", e.target.value)}
+                    />
+                    <InputComponent
+                        id="nationalNumber"
+                        type="nationalNumber"
+                        name="nationalNumber"
+                        label={translate("createNewProfile.nationalNumber")}
+                        placeholder={translate("createNewProfile.exm") + "1234567"}
+                        register={register}
+                        rules={{ required: 'You must enter your first name.' }}
+                        errors={errors}
+                        setValueMethod={setValue}
+                        watch={watch}
+                        onChange={(e) => setValue("nationalNumber", e.target.value)}
+                    />
+                    <InputComponent
+                        id="address"
+                        type="address"
+                        name="address"
+                        label={translate("createNewProfile.address")}
+                        placeholder={translate("createNewProfile.enterTheAddress")}
+                        register={register}
+                        rules={{ required: 'You must enter your first name.' }}
+                        errors={errors}
+                        setValueMethod={setValue}
+                        watch={watch}
+                        onChange={(e) => setValue("address", e.target.value)}
+                    />
+
                     <div>
                         <label>{translate("createNewProfile.phoneNumber")}<span>*</span></label>
                         <div>
-                            <Dropdown value={selectedCountryCode} onChange={(e) => setSelectedCountryCode(e.value)} options={countryCode} optionLabel="name"
-                                placeholder="+962" className="countryCode" />
-                            <InputText placeholder={translate("createNewProfile.exm") + "1234567"} className="nationalNoInput" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
+                            <DropDownComponent
+                                id="countryCode"
+                                name="countryCode"
+                                register={register}
+                                watch={watch}
+                                setValueMethod={setValue}
+                                options={countryCode}
+                                optionLabel="name"
+                                errors={errors}
+                                onChange={(e) => setValue("countryCode", e.value as object)}
+                                setValue={{ name: '+962', code: 'ORD' }}
+                            />
+                            <InputComponent
+                                id="phoneNumber"
+                                type="phoneNumber"
+                                name="phoneNumber"
+                                placeholder={translate("createNewProfile.exm") + "1234567"}
+                                register={register}
+                                errors={errors}
+                                setValueMethod={setValue}
+                                watch={watch}
+                                onChange={(e) => setValue("phoneNumber", e.target.value)}
+                            />
                         </div>
                     </div>
-                    <div>
-                        <label>{translate("createNewProfile.email")}<span>*</span></label>
-                        <InputText placeholder={translate("loginPage.emailPlaceholder")} value={email} onChange={(e) => setEmail(e.target.value)} />
-                    </div>
+
+                    <InputComponent
+                        id="email"
+                        type="email"
+                        name="email"
+                        label={translate("createNewProfile.email")}
+                        placeholder={translate("loginPage.emailPlaceholder")}
+                        register={register}
+                        // rules={{ required: 'You must enter your first name.' }}
+                        errors={errors}
+                        setValueMethod={setValue}
+                        watch={watch}
+                        onChange={(e) => setValue("email", e.target.value)}
+                    />
                 </div>
 
                 <div className="uploaderContainer">
@@ -105,8 +177,8 @@ const CreateNewProfilePage = () => {
                 </div>
 
                 <div className="actionBtns">
-                    <div onClick={saveAndCloseFn} className="white_btnStyle">{translate("createNewProfile.saveAndClose")}</div>
-                    <div onClick={nextFn} className="btnStyle">{translate("createNewProfile.next")}</div>
+                    <ButtonComponent Class={'BtnCancel'} onClick={saveAndCloseFn}>{translate("createNewProfile.saveAndClose")}</ButtonComponent>
+                    <ButtonComponent Class={'btnStyle'} onClick={handleSubmit(handleLogin)}>{translate("createNewProfile.next")}</ButtonComponent>
                 </div>
 
             </div>
