5bd470da34bbca184205a32de8a313020dd95a93
Mohamed Abdel Moneim <Eachawy@gmail.com>
2025-06-19 21:05:22 +0400

    add bradcramp component

--- src/main/webapp/app/modules/dashboard/main-page/mainPage.tsx
+++ src/main/webapp/app/modules/dashboard/main-page/mainPage.tsx
@@ -1,4 +1,5 @@
 import { ButtonComponent } from '@eachawy/frontend-library';
+import BreadcrumbComponent from 'app/shared/components/breadcrumbs.Component/breadcrumb.component';
 import React from 'react';
 import { useNavigate } from 'react-router';
 
@@ -10,6 +11,24 @@ const MainPage = () => {
 
     return (
         <>
+            <BreadcrumbComponent
+                links={[
+                    {
+                        id: 'PAGE1',
+                        name: {
+                            en: 'Dashboard',
+                            ar: 'لوحة التحكم',
+                        },
+                    },
+                    {
+                        id: 'PAGE1',
+                        name: {
+                            en: 'View files',
+                            ar: 'عرض الملفات',
+                        },
+                    }
+                ]}
+            />
             <div className='sdg_page'>
                 <ButtonComponent onClick={() => { navigate('/create-file/create-new-profile'); }}>
                     انشاء ملف جديد
