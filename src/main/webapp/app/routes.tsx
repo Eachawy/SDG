fe97f088f34f2894905986aa5745fcfa0c260b4f
Mohamed Abdel Moneim <Eachawy@gmail.com>
2025-06-19 20:14:11 +0400

    test

--- src/main/webapp/app/routes.tsx
+++ src/main/webapp/app/routes.tsx
@@ -14,6 +14,7 @@ import SubfileDataSentPage from "./modules/new-profile/subfile-data-sent/subfile
 import PrivateRoute from "./shared/auth/private-route";
 import { AUTHORITIES } from 'app/config/constants';
 import ErrorPage from "./shared/error/error.page";
+
 const AppRoutes = () => {
   return (
     <div>
