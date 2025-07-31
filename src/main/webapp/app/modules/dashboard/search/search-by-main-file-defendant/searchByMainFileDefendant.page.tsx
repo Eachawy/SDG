7894ad47f607a595f43c59568b36f6d4b936a492
Mohamed Abdel Moneim <Eachawy@gmail.com>
2025-07-31 21:04:59 +0400

    Fix issue

--- src/main/webapp/app/modules/dashboard/search/search-by-main-file-defendant/searchByMainFileDefendant.page.tsx
+++ src/main/webapp/app/modules/dashboard/search/search-by-main-file-defendant/searchByMainFileDefendant.page.tsx
@@ -70,6 +70,7 @@ export const SearchByMainFileDefendant = () => {
     return (
         <>
             <BreadcrumbComponent
+                back={true}
                 links={[
                     {
                         id: 'PAGE1',
